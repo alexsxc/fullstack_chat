ac20762b59321f9737027402d3e07c509c2637fd
alexsx <alexandersolomatn@gmail.com>
2024-06-18 14:11:58 +0300

    installation react - redux and tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+          <h1 className="text-4xl font-bold text-blue-500">
+            Hello, Tailwind CSS!
+          </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
